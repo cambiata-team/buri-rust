a15a8023bfe21f0020f440284738446044fdab07
Aaron Stanek <amr.stanek@gmail.com>
2023-01-25 05:53:12 +0000

    Disambiguate tag constraints

--- rust/type_checker/src/constraints.rs
+++ rust/type_checker/src/constraints.rs
@@ -1,12 +1,22 @@
 use crate::GenericTypeId;
+use std::collections::HashMap;
 use typed_ast::ConcreteType;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-/// Constrain that a generic type be a tag union with a tag named `tag_name`,
-/// the contents of which have the types of `tag_contents_types`.
-pub struct SubTagConstraint {
+/// Constrain that a generic type is a tag union
+/// with a tag of given name whose payload has particular types.
+pub struct HasTagConstraint {
     pub tag_name: String,
     pub tag_content_types: Vec<GenericTypeId>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+/// Constraint that a generic type is a tag union whose tags are a subset
+/// of a given set of tags.
+pub struct TagAtMostConstraint {
+    /// The keys are the names of the tags in a tag union.
+    /// The values are the types of the tag payloads.
+    pub tags: HashMap<String, Vec<GenericTypeId>>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -29,9 +39,12 @@ pub struct HasMethodConstraint {
 pub enum Constraint {
     /// Constrain that a generic type be equal to some concrete type.
     EqualToConcrete(ConcreteType),
-    // Constrain that a generic type is a list whose contents have a particular type.
+    /// Constrain that a generic type is a list whose contents have a particular type.
     ListOfType(GenericTypeId),
-    SubTag(SubTagConstraint),
+    /// Constrain that a generic type is a tag union with at least a given set of tags.
+    HasTag(HasTagConstraint),
+    /// Constrain that a generic type is a tag union with at most a given set of tags.
+    TagAtMost(TagAtMostConstraint),
     HasField(HasFieldConstraint),
     HasMethod(HasMethodConstraint),
     /// Constrain that a generic type is a function with a given return type.
