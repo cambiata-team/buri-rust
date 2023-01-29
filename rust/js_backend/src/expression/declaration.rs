290deaf6bf95d207fd46a1b4296825ec7a67a22f
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-01-29 14:53:33 -0800

    Print JS document

--- rust/js_backend/src/expression/declaration.rs
+++ rust/js_backend/src/expression/declaration.rs
@@ -2,14 +2,9 @@ use crate::{expression::print_expression, identifier::print_identifier};
 use typed_ast::ConcreteDeclarationExpression;
 
 pub fn print_declaration(declaration: &ConcreteDeclarationExpression) -> String {
-    let export = if declaration.is_exported {
-        "export "
-    } else {
-        ""
-    };
     let identifier = print_identifier(&declaration.identifier);
     let value = print_expression(&declaration.value);
-    format!("{export}const {identifier}={value}")
+    format!("const {identifier}={value}")
 }
 
 #[cfg(test)]
@@ -24,7 +19,6 @@ mod test {
             expression_type: ConcreteType::default_integer_for_test(),
             identifier: ConcreteExpression::raw_identifier_for_test("foo"),
             value: ConcreteExpression::integer_for_test(42),
-            is_exported: false,
         };
         assert_eq!(print_declaration(&declaration), "const foo=42");
     }
@@ -35,19 +29,7 @@ mod test {
             expression_type: ConcreteType::default_string_for_test(),
             identifier: ConcreteExpression::raw_identifier_for_test("hello"),
             value: ConcreteExpression::string_for_test("world"),
-            is_exported: false,
         };
         assert_eq!(print_declaration(&declaration), "const hello=\"world\"");
-    }
-
-    #[test]
-    fn can_export_declarations() {
-        let declaration = ConcreteDeclarationExpression {
-            expression_type: ConcreteType::default_integer_for_test(),
-            identifier: ConcreteExpression::raw_identifier_for_test("foo"),
-            value: ConcreteExpression::integer_for_test(42),
-            is_exported: true,
-        };
-        assert_eq!(print_declaration(&declaration), "export const foo=42");
     }
 }
