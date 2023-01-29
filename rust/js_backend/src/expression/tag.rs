7b763ad5ed866a5a1a5d1682196a69f16fad551d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-01-29 13:29:17 -0800

    Remove tag output optimization that causes bugs in incremental builds

--- rust/js_backend/src/expression/tag.rs
+++ rust/js_backend/src/expression/tag.rs
@@ -1,54 +1,34 @@
-use typed_ast::{ConcreteTagExpression, ConcreteType};
+use typed_ast::ConcreteTagExpression;
 
 pub fn print_tag(tag: &ConcreteTagExpression) -> String {
-    let some_tags_have_content;
-    if let ConcreteType::TagUnion(tag_union_type) = &tag.expression_type {
-        some_tags_have_content = tag_union_type.some_tags_have_content;
-    } else {
-        panic!("Expected tag to have tag union type");
+    let mut output = String::new();
+    output.push_str("[\"");
+    output.push_str(&tag.name);
+    output.push('"');
+    if !tag.contents.is_empty() {
+        let contents = tag
+            .contents
+            .iter()
+            .map(super::print_expression)
+            .collect::<Vec<_>>()
+            .join(", ");
+        output.push(',');
+        output.push_str(&contents);
     }
-    if some_tags_have_content {
-        let mut output = String::new();
-        output.push_str("[\"");
-        output.push_str(&tag.name);
-        output.push('"');
-        if !tag.contents.is_empty() {
-            let contents = tag
-                .contents
-                .iter()
-                .map(super::print_expression)
-                .collect::<Vec<_>>()
-                .join(", ");
-            output.push(',');
-            output.push_str(&contents);
-        }
-        output.push(']');
-        output
-    } else {
-        format!("\"{}\"", tag.name)
-    }
+    output.push(']');
+    output
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use typed_ast::ConcreteExpression;
-
-    #[test]
-    fn tags_in_tag_unions_without_contents_are_strings() {
-        let tag = ConcreteTagExpression {
-            name: "foo".to_string(),
-            expression_type: ConcreteType::default_tag_union_for_test(false),
-            contents: vec![],
-        };
-        assert_eq!(print_tag(&tag), "\"foo\"");
-    }
+    use typed_ast::{ConcreteExpression, ConcreteType};
 
     #[test]
     fn tags_without_contents_are_arrays_when_some_tags_have_content() {
         let tag = ConcreteTagExpression {
             name: "foo".to_string(),
-            expression_type: ConcreteType::default_tag_union_for_test(true),
+            expression_type: ConcreteType::default_tag_union_for_test(),
             contents: vec![],
         };
         assert_eq!(print_tag(&tag), "[\"foo\"]");
@@ -58,7 +38,7 @@ mod test {
     fn tags_with_contents_are_arrays_when_some_tags_have_content() {
         let tag = ConcreteTagExpression {
             name: "foo".to_string(),
-            expression_type: ConcreteType::default_tag_union_for_test(true),
+            expression_type: ConcreteType::default_tag_union_for_test(),
             contents: vec![ConcreteExpression::integer_for_test(42)],
         };
         assert_eq!(print_tag(&tag), "[\"foo\",42]");
