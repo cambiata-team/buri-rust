a2325acb91e2f86ffaaf1f7dd8744101725fc07d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-01-31 06:58:34 -0800

    connect e2e tests

--- rust/js_backend/src/lib.rs
+++ rust/js_backend/src/lib.rs
@@ -9,13 +9,14 @@ mod literals;
 
 #[must_use]
 pub fn print_js_document(document: &TypedDocument<ConcreteType>) -> String {
-    let mut result = String::from("import 'packages/std/prelude/index.js'\n");
+    let mut result = String::from("import '@packages/std/prelude/index.js'\n");
     result.push_str(&print_imports(&document.imports));
     for declaration in &document.variable_declarations {
         result.push('\n');
         if declaration.is_exported {
             result.push_str("export ");
         }
+        result.push_str("const ");
         // TODO(B-218): Clean this up to use the print_declaration method instead
         // manually rewriting that code here.
         result.push_str(&declaration.declaration.identifier_name);
