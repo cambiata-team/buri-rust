11e3d2f608913bd3adacb59d994f5080c004d194
Aaron Stanek <amr.stanek@gmail.com>
2023-02-02 20:01:20 +0000

    Add test for integer addition

--- tests/js/valid/integers/unsigned.test.js
+++ tests/js/valid/integers/unsigned.test.js
@@ -1,4 +1,4 @@
-import { one, two } from "@tests/js/valid/integers/unsigned.mjs"
+import { one, onePlusTwo, two } from "@tests/js/valid/integers/unsigned.mjs"
 
 import { expect, it } from "bun:test"
 
@@ -8,4 +8,8 @@ it("a literal with the value of 1 should be equal to 1", () => {
 
 it("a literal with the value of 2 should be equal to 2", () => {
     expect(two).toBe(2)
+})
+
+it("literal one plus literal two should equal three", () => {
+    expect(onePlusTwo.valueOf()).toBe(3)
 })
