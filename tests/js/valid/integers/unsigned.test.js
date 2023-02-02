9e0b24edb6068a9918d70ed90b9bcd923ee7e5da
Aaron Stanek <amr.stanek@gmail.com>
2023-02-02 20:05:26 +0000

    Correct integer literal tests

--- tests/js/valid/integers/unsigned.test.js
+++ tests/js/valid/integers/unsigned.test.js
@@ -3,11 +3,11 @@ import { one, onePlusTwo, two } from "@tests/js/valid/integers/unsigned.mjs"
 import { expect, it } from "bun:test"
 
 it("a literal with the value of 1 should be equal to 1", () => {
-    expect(one).toBe(1)
+    expect(one.valueOf()).toBe(1)
 })
 
 it("a literal with the value of 2 should be equal to 2", () => {
-    expect(two).toBe(2)
+    expect(two.valueOf()).toBe(2)
 })
 
 it("literal one plus literal two should equal three", () => {
