dec5fe2ad7999a6669ce56e9749e97a97ba7bc83
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-02-04 11:02:34 -0800

    Add e2e tests for associativity

---
+++ tests/js/valid/integers/associativity.test.js
@@ -0,0 +1,104 @@
+import {
+    eightDivideLParenThreeDivideTwoRParen,
+    eightDivideThreeDivideTwo,
+    eightModFiveModTwo,
+    eightModLParenFiveModTwoRParen,
+    lParenEightDivideThreeRParenDivideTwo,
+    lParenEightModFiveRParenModTwo,
+    lParenOnePlusTwoRParenPlusThree,
+    lParenOneTimesTwoRParenTimesThree,
+    lParenTenMinusFiveRParenMinusTwo,
+    lParenTwoPowerThreeRParenPowerTwo,
+    onePlusLParenTwoPlusThreeRParen,
+    onePlusTwoPlusThree,
+    oneTimesLParenTwoTimesThreeRParen,
+    oneTimesTwoTimesThree,
+    tenMinusFiveMinusTwo,
+    tenMinusLParenFiveMinusTwoRParen,
+    twoPowerLParenThreePowerTwoRParen,
+} from "@tests/js/valid/integers/associativity.mjs"
+import { describe, expect, it } from "bun:test"
+
+describe("associative operators", () => {
+    describe("addition", () => {
+        it("1 + (2 + 3) == (1 + 2) + 3", () => {
+            expect(onePlusLParenTwoPlusThreeRParen.valueOf()).toBe(
+                lParenOnePlusTwoRParenPlusThree.valueOf()
+            )
+        })
+
+        it("addition is left associative by default: 1 + 2 + 3 == (1 + 2) + 3", () => {
+            expect(onePlusTwoPlusThree.valueOf()).toBe(
+                lParenOnePlusTwoRParenPlusThree.valueOf()
+            )
+        })
+    })
+
+    describe("multiplication", () => {
+        it("1 * (2 * 3) == (1 * 2) * 3", () => {
+            expect(oneTimesLParenTwoTimesThreeRParen.valueOf()).toBe(
+                lParenOneTimesTwoRParenTimesThree.valueOf()
+            )
+        })
+
+        it("multiplication is left associative by default: 1 * 2 * 3 == (1 * 2) * 3", () => {
+            expect(oneTimesTwoTimesThree.valueOf()).toBe(
+                lParenOneTimesTwoRParenTimesThree.valueOf()
+            )
+        })
+    })
+})
+
+describe("non-associative operators", () => {
+    describe("subtraction", () => {
+        it("10 - (5 - 2) != (10 - 5) - 2", () => {
+            expect(tenMinusLParenFiveMinusTwoRParen.valueOf()).not.toBe(
+                lParenTenMinusFiveRParenMinusTwo.valueOf()
+            )
+        })
+
+        it("subtraction is left associative by default: 10 - 5 - 2 == (10 - 5) - 2", () => {
+            expect(tenMinusFiveMinusTwo.valueOf()).toBe(
+                lParenTenMinusFiveRParenMinusTwo.valueOf()
+            )
+        })
+    })
+
+    describe("division", () => {
+        it("8 / (3 / 2) != (8 / 3) / 2", () => {
+            expect(eightDivideLParenThreeDivideTwoRParen.valueOf()).not.toBe(
+                lParenEightDivideThreeRParenDivideTwo.valueOf()
+            )
+        })
+
+        it("division is left associative by default: 8 / 3 / 2 == (8 / 3) / 2", () => {
+            expect(eightDivideThreeDivideTwo.valueOf()).toBe(
+                lParenEightDivideThreeRParenDivideTwo.valueOf()
+            )
+        })
+    })
+
+    describe("modulo", () => {
+        it("8 % (5 % 2) != (8 % 5) % 2", () => {
+            expect(eightModLParenFiveModTwoRParen.valueOf()).not.toBe(
+                lParenEightModFiveRParenModTwo.valueOf()
+            )
+        })
+
+        it("modulo is left associative by default: 8 % 5 % 2 == (8 % 5) % 2", () => {
+            expect(eightModFiveModTwo.valueOf()).toBe(
+                lParenEightModFiveRParenModTwo.valueOf()
+            )
+        })
+    })
+
+    describe("power", () => {
+        it("2 ** (3 ** 2) != (2 ** 3) ** 2", () => {
+            expect(twoPowerLParenThreePowerTwoRParen.valueOf()).not.toBe(
+                lParenTwoPowerThreeRParenPowerTwo.valueOf()
+            )
+        })
+
+        // TODO(B-226): Write a test that shows that power is _right_ associative by default.
+    })
+})
