b896173c245576306de73579ff6de3511f82db45
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-02-10 07:37:54 -0800

    Add multiplication e2e tests (Fixes B-239)

---
+++ tests/js/valid/integers/multiplication.test.js
@@ -0,0 +1,47 @@
+import {
+    negativeOneTimesNegativeTwo,
+    negativeOneTimesTwo,
+    negativeThreeTimesFour,
+    negativeThreeTimesNegativeFour,
+    oneTimesNegativeTwo,
+    oneTimesTwo,
+    threeTimesFour,
+    threeTimesNegativeFour,
+} from "@tests/js/valid/integers/multiplication.mjs"
+import { describe, expect, it } from "bun:test"
+
+describe("1 and 2", () => {
+    it("1 * 2 == 2", () => {
+        expect(oneTimesTwo.valueOf()).toBe(2)
+    })
+
+    it("1 * -2 == -2", () => {
+        expect(oneTimesNegativeTwo.valueOf()).toBe(-2)
+    })
+
+    it("-1 * 2 == -2", () => {
+        expect(negativeOneTimesTwo.valueOf()).toBe(-2)
+    })
+
+    it("-1 * -2 == 2", () => {
+        expect(negativeOneTimesNegativeTwo.valueOf()).toBe(2)
+    })
+})
+
+describe("3 and 4", () => {
+    it("3 * 4 == 12", () => {
+        expect(threeTimesFour.valueOf()).toBe(12)
+    })
+
+    it("3 * -4 == -12", () => {
+        expect(threeTimesNegativeFour.valueOf()).toBe(-12)
+    })
+
+    it("-3 * 4 == -12", () => {
+        expect(negativeThreeTimesFour.valueOf()).toBe(-12)
+    })
+
+    it("-3 * -4 == 12", () => {
+        expect(negativeThreeTimesNegativeFour.valueOf()).toBe(12)
+    })
+})
