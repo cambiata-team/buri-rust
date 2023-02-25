58822d77c6a59aba0018cfbed756844336006df1
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-02-24 17:46:15 -0800

    Test returning functions from functions (Fixes B-290)

---
+++ tests/js/valid/functions/closures.test.js
@@ -0,0 +1,16 @@
+import { addN } from "@tests/js/valid/functions/closures.mjs"
+import { describe, expect, it } from "bun:test"
+
+describe("addN", () => {
+    it("add 1", () => {
+        const add1 = addN(1)
+        expect(add1(1).valueOf()).toEqual(2)
+        expect(add1(2).valueOf()).toEqual(3)
+    })
+
+    it("add 2", () => {
+        const add2 = addN(2)
+        expect(add2(1).valueOf()).toEqual(3)
+        expect(add2(2).valueOf()).toEqual(4)
+    })
+})
