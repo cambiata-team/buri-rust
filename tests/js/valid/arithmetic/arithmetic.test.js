56d8cbc678d16df3881c525e46149938317d1532
Aaron Stanek <amr.stanek@gmail.com>
2023-02-27 00:15:57 +0000

    Add variable arithmetic tests

---
+++ tests/js/valid/arithmetic/arithmetic.test.js
@@ -0,0 +1,22 @@
+import { BsevenPlusThree, BsevenMinusThree, BsevenTimesThree, BsevenDividedThree, BdifferenceOfSevenTimesThreeAndSevenPlusThree } from "@tests/js/valid/arithmetic/arithmetic.mjs"
+import { expect, it } from "bun:test"
+
+it("two variables can be added", () => {
+    expect(BsevenPlusThree.valueOf()).toBe(10)
+})
+
+it("two variables can be subtracted", () => {
+    expect(BsevenMinusThree.valueOf()).toBe(4)
+})
+
+it("two variables can be multiplied", () => {
+    expect(BsevenTimesThree.valueOf()).toBe(21)
+})
+
+it("two variables can be divided", () => { 
+    expect(BsevenDividedThree.valueOf()).toBe(2)
+})
+
+it("variables assigned from the results of arithmetic operations can be used in other arithmetic operations", () => {
+    expect(BdifferenceOfSevenTimesThreeAndSevenPlusThree.valueOf()).toBe(11)
+})
