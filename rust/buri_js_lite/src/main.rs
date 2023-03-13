cc74e56fd8167a6b7b36e295b5c1c6fb85b75732
Aaron Stanek <amr.stanek@gmail.com>
2023-03-13 22:25:00 +0000

    commit main file also

--- rust/buri_js_lite/src/main.rs
+++ rust/buri_js_lite/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Buri JS Lite");
+use buri_js_lite::get_file_paths;
+use std::env;
+
+fn main() -> Result<(), String> {
+    let arguments: Vec<String> = env::args().collect();
+    let file_paths = match get_file_paths(&arguments) {
+        Ok(x) => x,
+        Err(e) => return Err(e),
+    };
+    Ok(())
 }
