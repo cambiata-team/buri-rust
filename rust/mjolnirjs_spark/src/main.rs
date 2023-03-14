95bbf81f5656cbb8806bb4e4361e2b80a309b859
Aaron Stanek <amr.stanek@gmail.com>
2023-03-14 18:14:48 +0000

    Add MjolnirJS Spark

---
+++ rust/mjolnirjs_spark/src/main.rs
@@ -0,0 +1,38 @@
+use compiler::compile_buri_file;
+use mjolnirjs_spark::get_file_paths;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> Result<(), String> {
+    let arguments: Vec<String> = env::args().collect();
+    let file_paths = get_file_paths(&arguments)?;
+    let compiled_output = match std::fs::read_to_string(&file_paths.source) {
+        Ok(x) => compile_buri_file(&x),
+        Err(e) => {
+            return Err(format!(
+                "Error reading source file {}: {e}",
+                file_paths.source
+            ))
+        }
+    }?;
+    let mut output = match File::create(&file_paths.destination) {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(format!(
+                "Error creating output file {}: {e}",
+                file_paths.destination
+            ))
+        }
+    };
+    match write!(output, "{compiled_output}") {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(format!(
+                "Error writing to output file {}: {e}",
+                file_paths.destination
+            ))
+        }
+    };
+    Ok(())
+}
